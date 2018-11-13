9ff0ce63cdbbb19d4aa2b4887f12f2bbcd876dd6
joelluong <luongdaitrungduong@gmail.com>
2018-11-13 19:29:16 +1100

    Adding Products Model, Products Details and Polish

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
+    url = models.TextField()
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
     votes_total = models.IntegerField(default=1)
